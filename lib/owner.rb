77c298f6fcc44dd0fdcccdeda5722fe69d034053
Evan <evanahouse@gmail.com>
2021-04-08 16:33:27 -0700

    completed assignment

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,74 @@
+require 'pry'
+require 'cat.rb'
+require 'dog.rb'
+
 class Owner
-  # code goes here
+  @@all = []
+  
+  attr_reader :name, :species
+
+  def initialize(name)
+    
+    @name = name
+    @species = 'human'
+    @@all << self
+    
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def cats
+    Cat.all.filter{|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.filter{|dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+  
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.each{|dog| dog.mood = "happy"}
+  end
+  
+  def feed_cats
+    self.cats.each{|cat| cat.mood = "happy"}
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
+  def sell_pets
+    self.dogs.each{|dog| 
+      dog.mood = "nervous"
+      dog.owner = nil
+    }
+    self.cats.each{|cat|
+      cat.mood = "nervous"
+      cat.owner = nil
+    }
+  end
+  
+
 end
